ab8017c3130debadd4fbe1fa8413d91201020e9c
kevinten <596823919@qq.com>
2022-06-21 08:04:56 +0800

    init

---
+++ src/main/java/group/rxcloud/data/AdmissionReviewMessageBodyReader.java
@@ -0,0 +1,33 @@
+package group.rxcloud.data;
+
+import io.fabric8.kubernetes.api.model.admission.v1.AdmissionReview;
+import io.fabric8.kubernetes.client.utils.Serialization;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Provider
+@Consumes(APPLICATION_JSON)
+public class AdmissionReviewMessageBodyReader implements MessageBodyReader<AdmissionReview> {
+
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return type == AdmissionReview.class;
+    }
+
+    @Override
+    public AdmissionReview readFrom(Class<AdmissionReview> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
+        // without the extra step of wrapping the unmarshal will hit an error.
+        return Serialization.unmarshal(new String(entityStream.readAllBytes()), AdmissionReview.class);
+    }
+}
---
+++ src/main/java/group/rxcloud/webhook/MutatingAdmissionController.java
@@ -0,0 +1,103 @@
+package group.rxcloud.webhook;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.KubernetesResource;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.admission.v1.AdmissionRequest;
+import io.fabric8.kubernetes.api.model.admission.v1.AdmissionResponseBuilder;
+import io.fabric8.kubernetes.api.model.admission.v1.AdmissionReview;
+import io.fabric8.kubernetes.api.model.admission.v1.AdmissionReviewBuilder;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbConfig;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.io.StringReader;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import static javax.json.bind.JsonbConfig.FORMATTING;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+/**
+ * @see <a href="https://github.com/vsevel/quarkus-admission-controller">webhook</a>
+ */
+@Path("/mutate")
+@Produces(APPLICATION_JSON)
+@Consumes(APPLICATION_JSON)
+public class MutatingAdmissionController {
+
+    private static final Logger log = LoggerFactory.getLogger(MutatingAdmissionController.class);
+
+    @POST
+    public AdmissionReview validate(AdmissionReview review) {
+        JsonbConfig jsonbConfig = new JsonbConfig()
+                .setProperty(FORMATTING, true);
+        Jsonb jsonb = JsonbBuilder.create(jsonbConfig);
+        log.info("received admission review: {}", jsonb.toJson(review));
+
+        AdmissionRequest request = review.getRequest();
+
+        AdmissionResponseBuilder responseBuilder = new AdmissionResponseBuilder()
+                .withAllowed(true)
+                .withUid(request.getUid());
+
+        KubernetesResource object = request.getObject();
+
+        if (request.getOperation().equals("CREATE") && object instanceof Deployment) {
+            Deployment deployment = (Deployment) object;
+            if (needsMutating(deployment)) {
+
+                JsonObject original = toJsonObject(deployment);
+
+                mutate(deployment);
+
+                JsonObject mutated = toJsonObject(deployment);
+
+                String patch = Json.createDiff(original, mutated).toString();
+                String encoded = Base64.getEncoder().encodeToString(patch.getBytes());
+                log.info("patching with {}", patch);
+
+                responseBuilder
+                        .withPatchType("JSONPatch")
+                        .withPatch(encoded);
+            }
+        }
+
+        AdmissionReview admissionReview = new AdmissionReviewBuilder().withResponse(responseBuilder.build()).build();
+        //fabric8io has v1beta1 ; newer versions (>v1.15) have v1
+        //both are identical regarding interface
+        admissionReview.setApiVersion(review.getApiVersion());
+        return admissionReview;
+    }
+
+    JsonObject toJsonObject(HasMetadata object) {
+
+        return Json.createReader(new StringReader(JsonbBuilder.create().toJson(object))).readObject();
+    }
+
+    boolean needsMutating(HasMetadata object) {
+        ObjectMeta metadata = object.getMetadata();
+        Map<String, String> labels = metadata.getLabels();
+        return labels == null || !labels.containsKey("foo");
+    }
+
+    void mutate(HasMetadata object) {
+        ObjectMeta metadata = object.getMetadata();
+        Map<String, String> labels = metadata.getLabels();
+        if (labels == null) {
+            labels = new HashMap<>();
+            metadata.setLabels(labels);
+        }
+        labels.put("foo", "bar");
+    }
+}
